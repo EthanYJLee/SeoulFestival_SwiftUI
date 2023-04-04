1316365b2477d68c780cd00134894726cf21f2db
soonhyong <mikekwon42@gmail.com>
2023-04-04 11:46:53 +0900

    [추가] 탭 바

--- festivalSeoul/festivalSeoul/ContentView.swift
+++ festivalSeoul/festivalSeoul/ContentView.swift
@@ -10,33 +10,33 @@ import SwiftUI
 // tab bar view 
 struct ContentView: View {
     var body: some View {
-        NavigationView{
-            VStack {
-                NavigationLink(destination: MainView()) {
-                    Text("홈페이지")
+        VStack {
+            TabView {
+                  MainView()
+                    .tabItem {
+                      Image(systemName: "1.square.fill")
+                      Text("홈페이지")
+                    }
+                 MapView()
+                    .tabItem {
+                      Image(systemName: "2.square.fill")
+                      Text("지도")
+                    }
+                  
+                SearchView()
+                    .tabItem {
+                      Image(systemName: "3.square.fill")
+                      Text("검색")
+                    }
+                
+                CalendarView()
+                    .tabItem {
+                      Image(systemName: "4.square.fill")
+                      Text("달력")
+                    }
                 }
-                .padding()
-                
-                NavigationLink(destination: MapView()) {
-                    Text("문화 행사 지도")
-                }
-                .padding()
-                
-                NavigationLink(destination: SearchView()) {
-                    Text("검색 기능 탑재한 리스트 뷰")
-                }
-                .padding()
-                
-                NavigationLink(destination: CalendarView()) {
-                    Text("달력으로 즐겨찾기 행사 보기")
-                }
-                .padding()
-                
-                
-                
-            }
-            
         }
+            
     }
 }
 
